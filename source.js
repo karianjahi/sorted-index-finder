77287b7b33406a140530c71f5e94a433d69b7df2
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-18 15:44:23 +0100

    Add comments to the function and start implementing the getIndexToIns function

---
+++ source.js
@@ -0,0 +1,24 @@
+/*
+In this lab we are creating a function that returns the lowest index at which a value should be inserted into an array once it has been sorted in ascending order.
+
+User Stories:
+
+- We create a `getIndexToIns` function that takes two arguments: an array and a number.
+- We use the `sort` method to sort the array in ascending order.
+- The `getIndexToIns` function returns the lowest index at which the number should be inserted by using the `findIndex` method.
+- The `getIndexToIns` function always returns a number.
+- The `findIndex` method is a built-in array method in JavaScript. It takes a callback function and returns the index of the first element that satisfies the condition. Both `findIndex` and `sort` are higher-order functions.
+
+Examples:
+
+getIndexToIns([1, 2, 3, 4], 1.5) should return 1 because 1.5 is greater than 1 (index 0) and less than 2 (index 1).
+getIndexToIns([20, 3, 5], 19) should return 2 because after sorting to [3, 5, 20], 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+*/
+
+const getIndexToIns = (anArray, aNumber) => {
+    anArray.sort((a, b) => a - b);
+    return anArray;
+}
+
+console.log(getIndexToIns([7, 5, 4, 12, 15, 8], 10));
